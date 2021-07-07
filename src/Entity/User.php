fe979419861607dd65ff7a0790100e2d14a9c6b7
Evgenii Ryshkov <eryshkov@gmail.com>
2021-07-07 22:54:48 +0300

    08 - symfony updated

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -157,4 +157,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string) $this->getEmail();
     }
+
+    public function getUsername(): string
+    {
+        return (string) $this->getEmail();
+    }
 }
