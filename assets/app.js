aaf4dd8cac8974baecc3b95496b794d289b0b12c
Evgenii Ryshkov <eryshkov@gmail.com>
2021-07-07 23:51:35 +0300

    09 - creaned preview

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,27 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+const findCacheControlMeta = () => {
+    return document.querySelector('meta[name="turbo-cache-control"]');
+}
+
+document.addEventListener('show.bs.modal', () => {
+    if (findCacheControlMeta()) {
+        return;
+    }
+
+    const meta = document.createElement('meta');
+    meta.name = 'turbo-cache-control';
+    meta.content = 'no-cache';
+    meta.dataset.removable = true;
+    document.querySelector('head').appendChild(meta);
+});
+
+document.addEventListener('hidden.bs.modal', () => {
+    const meta = findCacheControlMeta();
+    if (!meta || !meta.dataset.removable) {
+        return;
+    }
+    meta.remove();
+});
